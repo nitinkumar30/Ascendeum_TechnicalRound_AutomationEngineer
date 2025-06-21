25d8c5c986edbbcaeaefc0e5e11278f627a2ee38
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2025-06-21 21:53:58 +0530

    Uncommented window-size

--- Round2.py
+++ Round2.py
@@ -19,7 +19,7 @@ chromedriver_autoinstaller.install()  # Check if the current version of chromedr
 
 chrome_options = webdriver.ChromeOptions()
 options = [
-    # "--window-size=1200,1200",
+    "--window-size=1200,1200",
     "--ignore-certificate-errors"
     "--disable-gpu",
     "--no-sandbox",
